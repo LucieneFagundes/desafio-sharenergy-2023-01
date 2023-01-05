e558d2e7ec127cf736c05c5820edcc32a56b5aba
LucieneFagundes <lfnascimento99@gmail.com>
2023-01-05 14:45:03 -0300

    impl: page login and page register

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,52 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
 
-import { Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Login from './pages/login/login';
+import Register from './pages/register/register';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="web" />
-      <div />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
+    <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-      {/* END: routes */}
-    </>
+    </BrowserRouter>
   );
 }
 
